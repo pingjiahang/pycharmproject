212e5caef67ec93510b6365a3bcefc59e0abc98b
unknown <you@example.com>
2020-04-07 08:48:46 +0800

    finished function

--- post.py
+++ post.py
@@ -1,14 +1,30 @@
 import requests
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15848417180161'
+
+
+def get_sign():
+    return '039b6c51c66ba62540119833ad93d999'
+
+
+def get_ts():
+    import time
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts
+
 form_data={
     'i':'我和你',
     'from':'AUTO',
     'to':'AUTO',
     'smartresult':'dict',
     'client':'fanyideskweb',
-    'salt':'15848417180161',
-    'sign':'039b6c51c66ba62540119833ad93d999',
-    'ts':'1584841718016',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv':'cc652a2ad669c22da983a705e3bca726',
     'doctype':'json',
     'version':'2.1',
@@ -16,5 +32,5 @@ form_data={
     'action':'FY_BY_REALTlME',
 }
 
-response=requests.post(url,form_data)
+response=requests.post(url,data=form_data)
 print(response.text)
